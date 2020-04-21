65c932eb0dd496cc5cc8b080e6c4b60bd79044af
Shreshth N. Singh <shreshthns.425@gmail.com>
2020-04-21 22:18:02 +0530

    initial commit

--- sketch.js
+++ sketch.js
@@ -1,64 +1,111 @@
-const Engine = Matter.Engine;
-const World= Matter.World;
-const Bodies = Matter.Bodies;
 
-var engine, world;
-var box1, pig1;
-var backgroundImg,platform;
+//NameSpacing Matter.js functions
+    const Engine = Matter.Engine;
+    const World= Matter.World;
+    const Bodies = Matter.Bodies;
 
-function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
-}
+
+//Creating variables related to Matter.js
+    var engine, world;
+
+
+//Creating variables related to animation
+    var roof, bob;
+
 
 function setup(){
-    var canvas = createCanvas(1200,400);
-    engine = Engine.create();
-    world = engine.world;
+
+    //Create canvas of 1200 width and 400 height
+        createCanvas(400,500);
 
 
-    ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
+    //Creating engine and world
+        engine = Engine.create();
+        world = engine.world;
+    
+    
+    //Giving options for roof
+        var roof_options={
+            isStatic:true
+        }    
+    
+    
+    //creating roof body
+        roof=Bodies.rectangle(200,50,400,10,roof_options);
+        World.add(world,roof);
+    
+    
+    //giving options for bob
+        var bob_options={
+            restituition:1.0,
+            density:1.5
+        }
+    
+    
+    //creating bob body
+        bob=Bodies.circle(200,150,20,bob_options);
+        World.add(world,bob);
+    
+    
+    //giving options for constraints
+        var options={
+            bodyA:bob,
+            bodyB:roof,
+            stiffness:0.004,
+            length:200
+        }
+    
+    
+    //constraining bob and roof
+            var chain=Matter.Constraint.create(options);
+            World.add(world,chain);
+    }
 
-    box1 = new Box(700,320,70,70);
-    box2 = new Box(920,320,70,70);
-    pig1 = new Pig(810, 350);
-    log1 = new Log(810,260,300, PI/2);
 
-    box3 = new Box(700,240,70,70);
-    box4 = new Box(920,240,70,70);
-    pig3 = new Pig(810, 220);
+function draw(){ 
+    //BlackBackground
+        background(0);
+    
+    
+    //Updating Engine
+        Engine.update(engine);
 
-    log3 =  new Log(810,180,300, PI/2);
 
-    box5 = new Box(810,160,70,70);
-    log4 = new Log(760,120,150, PI/7);
-    log5 = new Log(870,120,150, -PI/7);
+    rectMode(CENTER);
+    
+    fill("blue");
+    
+    //Text
+        textSize(20)
+        text("Press Space to Set your pendulum",40,300)
+        textSize(19)
+        text("Press any other key to release your pendulum",0,350)
+        text("Press Enter to restart your pendulum",40,400)
 
-    bird = new Bird(100,100);
-
-}
-
-function draw(){
-    background(backgroundImg);
-    Engine.update(engine);
-    console.log(box2.body.position.x);
-    console.log(box2.body.position.y);
-    console.log(box2.body.angle);
-    box1.display();
-    box2.display();
-    ground.display();
-    pig1.display();
-    log1.display();
-
-    box3.display();
-    box4.display();
-    pig3.display();
-    log3.display();
-
-    box5.display();
-    log4.display();
-    log5.display();
-
-    bird.display();
-    platform.display();
+    //Drawing the bodies
+        rect(roof.position.x,roof.position.y,400,20);
+        fill("white")
+        ellipse(bob.position.x,bob.position.y,50);
+    
+    
+    //Drawing the string of Pendulum
+        stroke("green");
+        strokeWeight(3);
+        line(bob.position.x,bob.position.y,roof.position.x,roof.position.y);
+    
+    
+    noStroke();
+    
+    
+    //Letting the user set the mouse X and mouse Y when key 32 is pressed; i.e-Space
+        if(keyCode===32){
+                bob.position.y = mouseY;
+            bob.position.x = mouseX;
+        }
+    
+    //Reset bob position    
+    if(keyCode===ENTER){
+        bob.position.x=200
+        bob.position.y=200
+    }
 }
